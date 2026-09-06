a1a3e3a51ec58b4a95e889fb459e69be5ad337bd
Mahfuz S. Shifat <mss.mahfuz.official@gmail.com>
2026-09-06 08:27:34 +0600

    V1.5

---
+++ Codes/Functions.py
@@ -0,0 +1,20 @@
+# def fumction_name(parameters):
+
+def add(a,b):
+    print("Additon = ", a+b)
+
+result = add(20,20)
+
+
+
+def multi(c,d):
+    print("Multiplicaton = ", c*d)
+
+result = multi(5,6)
+
+
+def division(k,l):
+    print("Division =", k/l)
+
+result = division(20,5)
+
