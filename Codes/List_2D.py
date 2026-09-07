43b5d9f575a5ff50674f841a64f2e78b3423be84
Mahfuz S. Shifat <mss.mahfuz.official@gmail.com>
2026-09-08 01:11:33 +0600

    V1.8

---
+++ Codes/List_2D.py
@@ -0,0 +1,18 @@
+my_matrix = [
+[ 1,4, 5,12],
+[-5,8, 9, 0],
+[-6, 7,11, 19]
+
+]
+
+spec_col = []
+for row in my_matrix:
+    spec_col.append(row[2])
+
+print(spec_col)
+
+
+
+items = [[1, 2, 5], [2, [5,7, [2,4,6]]]]
+
+print(items [1] [1] [2][2])
