6af37c5714318a40fb9aacbaac2441a6f11bdc85
Mahfuz S. Shifat <mss.mahfuz.official@gmail.com>
2026-09-20 07:08:52 +0600

    v1.9.2

--- Codes/Functions.py
+++ Codes/Functions.py
@@ -1,20 +1,51 @@
 # def fumction_name(parameters):
 
-def add(a,b):
-    print("Additon = ", a+b)
+# def add(a,b):
+#     print("Additon = ", a+b, "\n")
 
-result = add(20,20)
-
-
-
-def multi(c,d):
-    print("Multiplicaton = ", c*d)
-
-result = multi(5,6)
+# result = add(20,20)
 
 
-def division(k,l):
-    print("Division =", k/l)
 
-result = division(20,5)
+# def multi(c,d):
+#     print("Multiplicaton = ", c*d, "\n")
+
+# result = multi(5,6)
+
+
+# def division(k,l):
+#     print("Division =", k/l, "\n")
+
+# result = division(20,5)
+
+
+def result_check(marks):
+    if marks >= 40 :
+        return "Passed !"
+    else :
+        return "Failed !"
+
+marks = int(input("Enter Your Marks : "))
+result = result_check(marks)
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
